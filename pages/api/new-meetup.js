6d9d708e7301475da59fe265299b48c4ab07cfc1
Ali Salami <87864267+aleesalami932@users.noreply.github.com>
2022-01-28 17:23:33 +0200

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -8,7 +8,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      'mongodb+srv://alisalami:alisalami123@cluster0.ux88j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+      ''
     );
     const db = client.db();
 
